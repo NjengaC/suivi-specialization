6eee6f3a76212d734cb6e7b98a099c339d4cd0c7
yvonnegichovi <yvonnegichovi@gmail.com>
2024-08-11 15:53:31 +0300

    Map routing

--- entry/models.py
+++ entry/models.py
@@ -90,6 +90,7 @@ class Parcel(db.Model):
 
 
 
+
 class Admin(db.Model, UserMixin):
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     username = db.Column(db.String(100), unique=True, nullable=False)
