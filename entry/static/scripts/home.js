44d7151b1f6d3f80a5c38292a1b8b0841feb555c
NjengaC <victorcyrus01@gmail.com>
2024-08-14 03:25:50 +0300

    Modified rider and users dashboard to include a collapsable sidebar, also added greetings to user dashboard

--- entry/static/scripts/home.js
+++ entry/static/scripts/home.js
@@ -1,3 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Get the greeting element
+  const greetingElement = document.querySelector('.greeting-message');
+
+  // Get the current hour
+  const currentHour = new Date().getHours();
+
+  // Define the greeting messages
+  let greetingMessage = '';
+  if (currentHour < 12) {
+    greetingMessage = 'Good Morning! {{ current_user.name}}';
+  } else if (currentHour < 18) {
+    greetingMessage = 'Good Afternoon! {{ current_user.name}}';
+  } else {
+    greetingMessage = 'Good Evening! {{ current_user.name}}';
+  }
+  const h1Element = document.createElement('h1');
+  h1Element.textContent = greetingMessage;
+
+  greetingElement.innerHTML = '';
+  greetingElement.appendChild(h1Element);
+});
+
 // Wait for the DOM to be fully loaded before executing the code
 document.addEventListener("DOMContentLoaded", function() {
     // Get references to the elements you want to add click event listeners to
